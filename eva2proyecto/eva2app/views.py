f053949f6a8f32752cd429c9bdc4ce24fabaff4a
Anthhhony <maldonadocampusanoanthy@gmail.com>
2024-10-21 12:25:14 -0300

    clase2

--- eva2proyecto/eva2app/views.py
+++ eva2proyecto/eva2app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from eva2app.models import DBproyecto
 from . import forms
 
@@ -9,7 +9,6 @@ def registros(request):
 
 def agregar(request):
     form = forms.formulario()
-
     if request.method == "POST":
         form = forms.formulario(request.POST)
         if form.is_valid():
@@ -20,6 +19,24 @@ def agregar(request):
         db.save()
     data = {"form":form}
     return render(request,"templatesApp/agregar.html", data)
+
+def eliminar(request, idf):
+    proyecto = DBproyecto.objects.get(id = idf)
+    proyecto.delete()
+    return redirect("/")
+
+
+def actualizar(request, idf):
+    project = DBproyecto.objects.get(id = idf)
+    form = forms.formulario(instance = project)
+    if request.method == "POST":
+        form = forms.formulario(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+        return registros(request)
+    data = {"form":form}
+    return render(request, "templateApp/agregar.html", data)
+
 
 
 
